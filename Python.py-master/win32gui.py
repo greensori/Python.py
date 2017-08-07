6429cc24ef53de4a71a9ac98e79f7216ba3d4f8f
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-08-07 23:15:24 +0900

    Delete win32gui.py

--- Python.py-master/win32gui.py
+++
@@ -1,8 +0,0 @@
-import win32gui
-
-#getting mouse position
-flags, hcursor, (x, y) = win32gui.GetCursorInfo()
-
-#getting foreground window title name
-wname = win32gui
-wname.GetWindowText (wname.GetForegroundWindow())
