3ca9b8cbf2dc99a28f444406682e8ac58dc9f836
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-08-07 15:20:30 +0900

    Update win32gui.py

--- win32gui.py
+++ win32gui.py
@@ -2,3 +2,7 @@ import win32gui
 
 #getting mouse position
 flags, hcursor, (x, y) = win32gui.GetCursorInfo()
+
+#getting foreground window title name
+wname = win32gui
+wname.GetWindowText (wname.GetForegroundWindow())
