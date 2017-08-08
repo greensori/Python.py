cbd36e04f2de1f49432677ee88849362abfdfc1d
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-08-08 17:10:21 +0900

    Create cannyEdge(blackbg).py

---
+++ opencv/cannyEdge(blackbg).py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+from PIL import ImageGrab
+
+xsize = (0, 1250)
+ysize = (150, 1055)
+
+def canny(screen):
+    img_gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.Canny(img_gray, threshold1=200, threshold2=300)
+    return processed_img
+    
+while(True):
+    screen = np.array(ImageGrab.grab(bbox=(xsize[0], ysize[0], xsize[1], ysize[1])))
+    cv2.imshow('window test', new_screen)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+        
