d61b4300ce0d8a8d0246438250a869b5f11f74a0
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-10-13 09:24:06 +0900

    Update contours.py

--- opencv/contours.py
+++ opencv/contours.py
@@ -23,17 +23,9 @@ def boundaryrec():
     thresh = cv2.adaptiveThreshold(blur,255,1,1,11,2)
     #ret, thresh = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = contours[1509]
-    '''
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-    '''
-    x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    print len(contours)
-    print (cnt)
-    print len(cnt)
     cv2.imwrite('testsample\pp_bdThres.png',thresh)
     cv2.imwrite('testsample\pp_boundary.png',img)
     print ('complete')
